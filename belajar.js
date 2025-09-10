dff89855af9611f963ce10e276b90f4c15c44c88
tyo erlangga <aerlggaa@gmail.com>
2025-09-10 14:38:42 +0700

    latihan bisdig

--- belajar.js
+++ belajar.js
@@ -1,2 +1,13 @@
 <script src="belajar.js"></script>
+    <script>  
+    alert("Haloww!");
+
+let konfirmasi = confirm("Tutor JavaScript dek ?");
+if (konfirmasi) {
+let umur = prompt("Berapa umurmu?");
+alert("udah tua "  + "yaa!");
+} else {
+alert("it's okey");
+}
+    </script>
 
